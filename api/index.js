cace8f226ff5e47e60fc445b08dd17b2ad54e53e
tarunarora289 <tarun.arora289@gmail.com>
2026-04-23 23:17:38 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, x-admin-token');
 
   if (req.method === 'OPTIONS') {
     res.status(200).end();
